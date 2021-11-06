1bca886194b75c72e4e12d9d3567a926a680a2c3
innazhurba <inazhurba@gmail.com>
2021-11-06 14:02:30 +0200

    add task 8 - find max profit

--- ASD_sem7/Program.cs
+++ ASD_sem7/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
 using ASD_sem7.task_7;
+using ASD_sem7.task_8;
 
 namespace ASD_sem7
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Call_task7()
         {
             scheduling schedule = new scheduling();
             int[][] a = new int[][] {
@@ -29,6 +30,21 @@ namespace ASD_sem7
                 
             };*/
             Console.WriteLine(schedule.solve7(a));
+        }
+
+        public static void Call_task8()
+        {
+            MaxProfit maxProfit = new MaxProfit();
+            //int []arr = { 10,1,2,1,4,3}; //4
+            int []arr = { 5,10,15,20}; //15
+            
+            Console.WriteLine(maxProfit.maxProfit(arr));
+        }
+        static void Main(string[] args)
+        {
+            //Call_task7();
+            
+            Call_task8();
         }
     }
 }
---
+++ ASD_sem7/task 8/MaxProfit.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace ASD_sem7.task_8
+{
+    public class MaxProfit
+    {
+        public int maxProfit(int[] arr)
+        {
+            int profit = 0;
+            
+            //n
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[i - 1]) 
+                    profit += arr[i] - arr[i - 1];
+            }
+            return profit;
+        }
+    }
+}
