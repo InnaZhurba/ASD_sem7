f8e534432704e844572f95ecb6a052b75871e34d
innazhurba <inazhurba@gmail.com>
2021-11-06 13:16:54 +0200

    add complexity comments in scheduling task 7

--- ASD_sem7/task 7/scheduling.cs
+++ ASD_sem7/task 7/scheduling.cs
@@ -5,19 +5,23 @@ namespace ASD_sem7.task_7
 {
     public class scheduling
     {
+        //a[n][m] => n*m matrix
         public int solve7(int[][] a)
         {
             int nDeleteEl = 0;
             int[] arr = new int[a.GetLength(0)];
             MergeSort mergeSort = new MergeSort();
 
+            //n
             for (int i = 0; i < a.GetLength(0); i++)
             {
                 arr[i] = a[i][1];
             }
             
+            //nlogn
             mergeSort.devideANDconquare(arr,0,a.GetLength(0)-1,a);
-
+            
+            //n
             for (int i = 1; i < a.GetLength(0); i++)
             {
                 if (a[i][0] < a[i - 1][1])
